b2fadd5a4dc9648da4aaabf8652bfb7f810eab2e
martin <m@rtin.bz>
2023-02-02 19:02:33 -0300

    .

---
+++ lang/lambdathis.cpp
@@ -0,0 +1,42 @@
+//
+// Created by martin on 02/02/2023.
+//
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <utility>
+
+using namespace std;
+using namespace literals;
+
+class Data
+{
+ public:
+  explicit Data (string name) :
+      name(std::move(name))
+  {
+
+  }
+
+  [[nodiscard]] auto start_thread_with_copy_of_this () const
+  {
+    thread t { [*this] ()
+               {
+                   this_thread::sleep_for(3s);
+                   cout << name << endl;
+               } };
+    return t;
+  }
+
+ private:
+  string name;
+};
+
+int main ()
+{
+  thread t;
+  Data d { "C1" };
+  t = d.start_thread_with_copy_of_this();
+  t.join();
+}
