13c21048e2c5d91398416aeb77d05cc6ff3822a5
martin <m@rtin.bz>
2023-02-03 20:44:14 -0300

    .

---
+++ tmpl/ifruntime.cpp
@@ -0,0 +1,20 @@
+//
+// Created by martin on 03/02/2023.
+//
+
+#include "ifruntime.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  /*
+   * ERROR: In template: no viable conversion from returned value of type 'int'
+   * to function return type 'string' (aka 'basic_string<char>')
+   */
+
+  //  cout << as_string(42) << '\n';
+  //  cout << as_string(string("hello")) << '\n';
+  //  cout << as_string("hello") << '\n';
+}
---
+++ tmpl/ifruntime.hpp
@@ -0,0 +1,26 @@
+//
+// Created by martin on 03/02/2023.
+//
+
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+template < typename T >
+string as_string(T x)
+{
+  if (is_same_v<T, string>)
+    {
+      return x;
+    }
+  else if  (is_arithmetic_v<T>)
+    {
+      return to_string(x);
+    }
+  else
+    {
+      return string(x);
+    }
+}
