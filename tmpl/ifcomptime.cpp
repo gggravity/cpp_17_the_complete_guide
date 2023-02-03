0f2979b1f821d950c45b2125819da1c4c10f8058
martin <m@rtin.bz>
2023-02-03 20:33:59 -0300

    .

---
+++ tmpl/ifcomptime.cpp
@@ -0,0 +1,15 @@
+//
+// Created by martin on 03/02/2023.
+//
+
+#include "ifcomptime.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << as_string(42) << '\n';
+  cout << as_string(string("hello")) << '\n';
+  cout << as_string("hello") << '\n';
+}
