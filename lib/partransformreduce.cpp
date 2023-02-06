a994c7a363d5a7e73ff572dcfb1b3f68d4b8dd6a
martin <m@rtin.bz>
2023-02-06 19:48:44 -0300

    .

--- lib/partransformreduce.cpp
+++ lib/partransformreduce.cpp
@@ -23,15 +23,13 @@ void print_sum (long num)
       return val * val;
   };
 
-  auto sum {
-      transform_reduce(
-          execution::par,
-          coll.begin(),
-          coll.end(),
-          0L,
-          plus { },
-          sum_val)
-  };
+  auto sum { transform_reduce(
+      execution::par,
+      coll.begin(),
+      coll.end(),
+      0L,
+      plus { },
+      sum_val) };
 
   cout << "reduce(): " << sum << '\n';
 }
