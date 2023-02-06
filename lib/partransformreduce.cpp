49f4a06d88993ee5ac4ddee057a1da6933a4bee8
martin <m@rtin.bz>
2023-02-06 19:47:08 -0300

    .

---
+++ lib/partransformreduce.cpp
@@ -0,0 +1,46 @@
+//
+// Created by martin on 2/6/23.
+//
+
+#include <bits/stdc++.h>
+#include <execution>
+#include "Timer.hpp"
+
+using namespace std;
+
+void print_sum (long num)
+{
+  vector<long> coll;
+  coll.reserve(4 * num);
+
+  for (long i { 0 } ; i < num ; ++i)
+    {
+      coll.insert(coll.end(), { 1, 2, 3, 4 });
+    }
+
+  auto sum_val = [] (auto val)
+  {
+      return val * val;
+  };
+
+  auto sum
+      {
+          transform_reduce(
+              execution::par,
+              coll.begin(),
+              coll.end(),
+              0L,
+              plus { },
+              sum_val)
+      };
+
+  cout << "reduce(): " << sum << '\n';
+}
+
+int main ()
+{
+  print_sum(1);
+  print_sum(1'000);
+  print_sum(1'000'000);
+  print_sum(10'000'000);
+}
