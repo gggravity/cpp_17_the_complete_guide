d74c1a3730e533dcacaf42cee7a92c7e60909909
Martin Eriksen <m@rtin.bz>
2023-02-08 21:16:14 -0300

    .

---
+++ lib/sample1.cpp
@@ -0,0 +1,21 @@
+//
+// Created by martin on 2/8/23.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  vector<string> collection;
+
+  for (int i { 0 } ; i < 10'000 ; ++i)
+    {
+      collection.push_back("value" + to_string(i));
+    }
+  sample(collection.begin(), collection.end(),
+         ostream_iterator<string> { cout, "\n" },
+         10,
+         default_random_engine { });
+}
